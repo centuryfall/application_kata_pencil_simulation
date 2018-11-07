c9fc867d4c0a00b57a6e34b9dc263c2079d5f62a
Zach Hamm <zjhamm304@gmail.com>
2018-11-07 13:32:13 -0500

    Update pencil.rb

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -1,1 +1,14 @@
+class Pencil
 
+  def initialize
+    @degradation_value
+  end
+
+  def set_degradation_value(val)
+    @degradation_value = val
+  end
+
+  def get_degradation_value
+    @degradation_value
+  end
+end
