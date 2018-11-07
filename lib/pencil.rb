5c7600def3a78bb1500e8942dee72d89a35fc62d
Zach Hamm <zjhamm304@gmail.com>
2018-11-07 15:34:53 -0500

    Update pencil.rb
    Point degradation 1.0

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -15,19 +15,23 @@ class Pencil
 
   #Decrases the value of the graphite point depending on the type of character given
   def decrease_degradation_value(given_character)
-    if given_character =~ (/^(\s|\n)$/)
-    elsif given_character =~ (/^[[:upper:]]$/)
-      self.set_degradation_value(self.get_value(:degradation_value) - 2)
-    elsif given_character =~ (/^[[:lower:]]|\S$/) && given_character !~ (/^[[:upper:]]$/)
-      self.set_degradation_value(self.get_value(:degradation_value) - 1)
-    end
+    decrease_by =
+        if given_character =~ (/^[[:upper:]]$/) && given_character !~ (/^(\s|\n)$/)
+          2
+        elsif given_character =~ (/^[[:lower:]]|\S$/) && given_character !~ (/^[[:upper:]]$/) && given_character !~ (/^(\s|\n)$/)
+          1
+        else
+          0
+        end
+    self.set_degradation_value(self.get_value(:degradation_value) - decrease_by) unless self.get_value(:degradation_value) <= 0
   end
 
   #Writes text per each character. If the point value is 0, character returned is blank.
+  #Currently it's setting the string input equal to what it should be if the point value is 0
   def write(text)
     text.split("").each_with_index do |val, index|
+      text[index] = (self.get_value(:degradation_value) > 0) ? val : " "
       decrease_degradation_value(val)
-      text[index] = val
     end
     text
   end
