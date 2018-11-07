e0634348ab75aba6f19fc0f28df43f75d22c736d
Zach Hamm <zjhamm304@gmail.com>
2018-11-07 13:49:38 -0500

    Update pencil_spec.rb

--- spec/pencil_spec.rb
+++ spec/pencil_spec.rb
@@ -3,20 +3,30 @@ require './lib/paper.rb'
 
 describe 'pencil' do
 
-    point_degrade_value = 1000
+  point_degrade_value = 1000
 
-       #Change paper value to hash to store text better
-   it 'appends text to text already on paper' do
-     paper = Paper.new
-     pencil = Pencil.new
-     init_text = 'She sells sea shells'
-     test_string = ' down by the sea shore'
-     paper.set_text(init_text)
-     paper.set_text(pencil.write_to_paper(test_string))
+  #Writing to paper
+  it 'appends text to existing text already on paper' do
+    paper = Paper.new
+    pencil = Pencil.new
+    init_text = 'She sells sea shells'
+    test_string = ' down by the sea shore'
+    paper.set_text(init_text)
+    paper.set_text(pencil.write_to_paper(test_string))
 
-     expect(paper.get_text).to match(init_text+test_string)
-   end
+    expect(paper.get_text).to match(init_text + test_string)
+  end
 
+  it 'adds text to paper if the paper has no text' do
+    paper = Paper.new
+    pencil = Pencil.new
+    test_string = 'Hello, world!'
+    paper.set_text(pencil.write_to_paper(test_string))
+
+    expect(paper.get_text).to match(test_string)
+  end
+
+  #Graphite point degradation
   it 'has a value for point degradation' do
     pencil = Pencil.new
     pencil.set_degradation_value(point_degrade_value)
