3e6c373038d02979532d7384a616565e60c64616
Zach Hamm <zjhamm304@gmail.com>
2018-11-07 17:02:07 -0500

    Write method refactoring
    Refactored write method for better design principles.

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -35,16 +35,18 @@ class Pencil
 
   #Writes text per each character. If the point value is 0, character returned is blank.
   #Currently it's setting the string input equal to what it should be if the point value is 0
-  def write(text)
+  def write(text, to_paper)
     text.split("").each_with_index do |val, index|
       text[index] = (self.get_value(:degradation_value) > 0) ? val : " "
       decrease_degradation_value(val)
     end
-    text
+    to_paper.set_text(text)
   end
 
   def sharpen
-    self.set_length(self.get_value(:length) - 1) unless self.get_value(:length) <= 0
-    self.set_degradation_value(@max_point_grade) unless self.get_value(:length) <= 0
+    if self.get_value(:length) > 0
+      self.set_length(self.get_value(:length) - 1)
+      self.set_degradation_value(@max_point_grade)
+    end
   end
 end
